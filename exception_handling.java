45291d1c455f85b7cf5b96d8f81b7e31ccdbab87
Harsh Narayan Jha <50262541+HarshNarayanJha@users.noreply.github.com>
2024-10-27 18:59:22 +0530

    return with finally block

--- exception_handling.java
+++ exception_handling.java
@@ -3,8 +3,27 @@ public class exception_handling {
     public static void main(String[] args) {
         try {
             int myInt = Integer.parseInt("pants");
-        } catch (NumberFormatException nfe) {
+            System.out.println("THis won't print");
+        } catch (NumberFormatException | NullPointerException nfe) {
             System.err.println("not a number");
+        } catch (Exception e) {
+            System.err.println(e);
+        } finally {
+            System.out.println("Finalyy");
+        }
+
+        System.out.println("Last Line");
+
+        System.out.println(printANumber());
+    }
+
+    private static int printANumber() {
+        try {
+            return 3;
+        } catch (Exception e) {
+            return 4;
+        } finally {
+            return 5;
         }
     }
 }
